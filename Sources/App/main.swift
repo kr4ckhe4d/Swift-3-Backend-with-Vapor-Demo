51cf3b2ddd0a03ff86d78f092d7a0d93385f142b
Nipuna <nipuna@app-monkeyz.com>
2016-11-15 15:01:25 +0530

    Added view rendering

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,8 +1,22 @@
 import Vapor
 let drop = Droplet()
 
+// Simple routing
 drop.get("/"){ request in
     return "Hello World!"
+}
+
+// Passing params in routing
+drop.get("/name",":name"){ request in
+    if let name = request.parameters["name"]?.string{
+        return "Hello \(name)!"
+    }
+    return "Error retrieving parameters!"
+}
+
+// Returning a view
+drop.get("/view"){ request in
+    return try drop.view.make("sample.html")
 }
 
 drop.run()
